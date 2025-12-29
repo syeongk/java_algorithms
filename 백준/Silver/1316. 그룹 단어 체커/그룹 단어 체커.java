8421a6702cd28a934f6c62527667bd8bdf0d4b3e
Seoyoung Kim <syeongk7@gmail.com>
2025-12-29 19:00:59 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 68 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,8 +1,9 @@
 // 특정 단어(a)가 연속으로 나오는 것은 가능하지만
 // 다른 단어(b)가 나온 후에 등장하면 안 됨
 // 연속으로 등장하는지 확인(이전에 나왔던 문자 저장)해서 연속으로 나오는 것은 ㅇㅋ
-// 이때까지 나왔던 문자열 저장해서 또 나오면 ㄴㄴ, 바로 다음 줄로 ㄱㄱ
-// 줄 넘어가면 이전에 나왔던 문자열, 이때까지 나왔던 문자열 초기화
+// 이때까지 나왔던 문자 또 나오면 ㄴㄴ, 바로 다음 줄로 ㄱㄱ
+// 줄 넘어가면 이전에 나왔던 문자, 이때까지 나왔던 문자 초기화
+// 시간복잡도 N x L^2
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,22 +17,19 @@ public class Main {
         int ans = 0;
         for (int i=0; i<N; i++) {
             String word = br.readLine(); // 단어 입력 받기
-            char[] chars = new char[word.length()]; // 이때까지 나온 문자 배열 저장
 
-            chars[0] = word.charAt(0); // 단어 첫 글자 저장
             char checkChar = word.charAt(0); // 직전 문자 저장
             boolean isGroup = true;
             for (int j=1; j<word.length(); j++) {
                 if (checkChar != word.charAt(j)) {
-                // 이전에 같은 문자가 나왔는지 확인
-                for (int k=0; k<j; k++) {
-                    if (chars[k] == word.charAt(j)) { // 같은 문자가 나옴
-                        isGroup = false;
-                        break; // 그룹 단어에 해당하지 않음
+                    // 이전에 같은 문자가 나왔는지 확인
+                    for (int k=0; k<j; k++) {
+                        if (word.charAt(k) == word.charAt(j)) { // 같은 문자가 나옴
+                            isGroup = false;
+                            break; // 그룹 단어에 해당하지 않음
+                        }
                     }
-                }
                 // 같은 문자가 나오지 않았을 때
-                chars[j] = word.charAt(j);
                 checkChar = word.charAt(j);
                 }
             }
