73e5eaf5b488fd9776bfbcbeac5d08f36fe7a13e
Seoyoung Kim <syeongk7@gmail.com>
2025-12-30 18:01:29 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,78 +6,36 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String str = br.readLine();
-
-        StringBuilder cur = new StringBuilder();
-
-        String[] alphabets = {"c", "d", "l", "n", "s", "z"};
-        String[] cros = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
         int ans = 0;
 
-        int condition = 0;
-
         int i = 0;
         while (i < str.length()) {
-            if (condition == 1) {
-                i += 1;
-            } else if (condition == 2) {
-                i += 2;
-            } else if (condition == 3) {
+            // dz= 인 경우
+            if (i + 2 < str.length()
+                    && str.charAt(i) == 'd'
+                    && str.charAt(i + 1) == 'z'
+                    && str.charAt(i + 2) == '=') {
+                ans++;
                 i += 3;
             }
-
-            if (i >= str.length()) break;
-
-            condition = 0;
-
-            cur.setLength(0);
-            cur.append(str.charAt(i));
-
-            // c,d,l,n,s,z 로 시작하는지 확인
-            for (String alphabet : alphabets) {
-                if (cur.toString().equals(alphabet)) {
-
-                    if (i + 1 < str.length()) {
-                        cur.append(str.charAt(i + 1)); // cur : c=, nj
-
-                        // 크로아티아 알파벳에 해당하는지 확인
-                        for (String cro : cros) {
-                            if (cur.toString().equals(cro)) {
-                                ans++;
-                                condition = 2;
-                                break;
-                            }
-                        }
-                    } else {
-                        break;
-                    }
-
-                    // 크로아티아 알파벳(dz=)에 해당하는지 확인
-                    if (cur.toString().equals("dz")) {
-
-                        if (i + 2 < str.length()) {
-                            cur.append(str.charAt(i + 2));
-
-                            if (cur.toString().equals("dz=")) {
-                                ans++;
-                                condition = 3;
-                                break;
-                            }
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                // 크로아티아 알파벳을 찾은 경우
-                if (condition >= 2) {
-                    break;
-                }
-            }
-            // c,d,l,n,s,z 로 시작하지 않았을 때, 마지막 글자일 때
-            if (condition <= 1) {
+            else if (i + 1 < str.length()
+                    && ((str.charAt(i) == 'c' && (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-'))
+                    || (str.charAt(i) == 'd' && (str.charAt(i + 1) == '-'))
+                    || (str.charAt(i) == 'l' && (str.charAt(i + 1) == 'j'))
+                    || (str.charAt(i) == 'n' && (str.charAt(i + 1) == 'j'))
+                    || (str.charAt(i) == 's' && (str.charAt(i + 1) == '='))
+                    || (str.charAt(i) == 'z' && (str.charAt(i + 1) == '=')))
+            )
+            {
                 ans++;
-                condition = 1;
+                i += 2;
+            }
+            else {
+                ans++;
+                i += 1;
             }
         }
         System.out.println(ans);
